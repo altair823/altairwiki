740d9781629b44e1516a1bec20790f678cefe0c0
altair823 <kimth0734@gmail.com>
2024-08-24 04:19:25 +0900

    initial hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+// 기본 설정
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// 라우팅 정의
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+// 서버 실행
+app.listen(PORT, () => {
+    console.log(`Listen : ${PORT}`);
+});
